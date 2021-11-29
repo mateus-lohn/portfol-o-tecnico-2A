6b8ef71bd0834bb54e6f2dcc7f960f61892d29d0
mateus-lohn <83706786+mateus-lohn@users.noreply.github.com>
2021-11-29 19:22:13 -0300

    Create Gato.java

---
+++ logicaprogramaçao/PetShop/src/Gato.java
@@ -0,0 +1,71 @@
+/ *
+ * Para alterar este cabeçalho de licença, escolha Cabeçalhos de licença em Propriedades do projeto.
+* Para alterar este arquivo de modelo, escolha Ferramentas | Modelos
+ * e abra o modelo no editor.
+ * /
+pacote  petshop ;
+
+/ **
+ *
+* @author SESI_SENAI
+ * /
+public  class  Gato  extends  Animal {
+    castrado booleano ;
+
+    public  boolean  isCastrado () {
+        voltar castrado;
+    }
+
+    public  void  setCastrado ( boolean  castrado ) {
+        isso . castrado = castrado;
+    }
+
+    public  int  getMatricula () {
+        return matricula;
+    }
+
+    public  void  setMatricula ( int  matricula ) {
+        isso . matrícula = matrícula;
+    }
+
+    public  String  getNome () {
+        return nome;
+    }
+
+    public  void  setNome ( String  nome ) {
+        isso . nome = nome;
+    }
+
+    public  String  getCor_pelagem () {
+        return cor_pelagem;
+    }
+
+    public  void  setCor_pelagem ( String  cor_pelagem ) {
+        isso . cor_pelagem = cor_pelagem;
+    }
+
+    public  String  getGenero () {
+        return genero;
+    }
+
+    public  void  setGenero ( String  genero ) {
+        isso . genero = genero;
+    }
+
+    public  int  getIdade () {
+        voltar idade;
+    }
+
+    public  void  setIdade ( int  idade ) {
+        isso . idade = idade;
+    }
+
+    public  float  getPeso () {
+        retornar peso;
+    }
+
+    public  void  setPeso ( peso flutuante  ) {
+        isso . peso = peso;
+    }
+       
+}
