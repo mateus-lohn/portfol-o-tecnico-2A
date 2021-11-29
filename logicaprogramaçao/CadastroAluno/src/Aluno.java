667fe88d30362a75623546b892709ee252593a74
mateus-lohn <83706786+mateus-lohn@users.noreply.github.com>
2021-11-29 19:10:36 -0300

    Create Aluno.java

---
+++ logicaprogramaçao/CadastroAluno/src/Aluno.java
@@ -0,0 +1,19 @@
+package  aula2a ;
+
+
+public  class  Aluno  extends  Pessoa {
+    Matrícula de cordas ;
+
+    public  Aluno ( String  matricula ) {
+        isso . matrícula = matrícula;
+    }
+
+    public  String  getMatricula () {
+        return matricula;
+    }
+
+    public  void  setMatricula ( String  matricula ) {
+        isso . matrícula = matrícula;
+    }    
+    
+}
