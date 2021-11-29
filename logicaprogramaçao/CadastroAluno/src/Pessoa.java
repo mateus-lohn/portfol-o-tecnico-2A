cb1ea3fdcc7567209c9bf6704fb968e752b75d39
mateus-lohn <83706786+mateus-lohn@users.noreply.github.com>
2021-11-29 19:11:58 -0300

    Create Pessoa.java

---
+++ logicaprogramaçao/CadastroAluno/src/Pessoa.java
@@ -0,0 +1,33 @@
+package  aula2a ;
+
+public  class  Pessoa {
+    Nome da string ;
+    String cpf;
+    int idade;
+
+    public  String  getNome () {
+        return nome;
+    }
+
+    public  void  setNome ( String  nome ) {
+        isso . nome = nome;
+    }
+
+    public  String  getCpf () {
+        return cpf;
+    }
+
+    public  void  setCpf ( String  cpf ) {
+        isso . cpf = cpf;
+    }
+
+    public  int  getIdade () {
+        voltar idade;
+    }
+
+    public  void  setIdade ( int  idade ) {
+        isso . idade = idade;
+    }
+    
+    
+}
