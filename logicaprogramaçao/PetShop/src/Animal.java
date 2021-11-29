32df6233b039caa31804acfd60c42444d9f0e961
mateus-lohn <83706786+mateus-lohn@users.noreply.github.com>
2021-11-29 19:20:33 -0300

    Create Animal.java

---
+++ logicaprogramaçao/PetShop/src/Animal.java
@@ -0,0 +1,21 @@
+ pacote  petshop ;
+
+/ *
+ * Para alterar este cabeçalho de licença, escolha Cabeçalhos de licença em Propriedades do projeto.
+* Para alterar este arquivo de modelo, escolha Ferramentas | Modelos
+ * e abra o modelo no editor.
+ * /
+
+/ **
+ *
+* @author SESI_SENAI
+ * /
+public  class  Animal {
+    int matricula;
+    Nome da string ;
+    String cor_pelagem;
+    String genero;
+    int idade;
+    peso flutuante ;
+    
+}
